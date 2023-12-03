5fc01e40f25d649c62d0b32882204e38b6f3e335
jorjiiie <ryanzhu.1@outlook.com>
2023-12-03 14:36:57 -0500

    finish day1

--- day1/main.ml
+++ day1/main.ml
@@ -31,10 +31,63 @@ let get_concat str =
   (String.make 1 (match_first str)) ^ (String.make 1 (match_last str 'x'))
   
 
+let nums = ["one";"two";"three";"four";"five";"six";"seven";"eight";"nine";]
 
+
+let prefix pref str =
+  if String.length pref > String.length str then false else
+  let pref_len = String.length pref in 
+  let first_half = String.sub str 0 pref_len in 
+  pref = first_half 
+
+let assoc_nums str = 
+  let rec fn cur s n =
+    match cur with 
+    | [] -> None
+    | num :: rest -> if prefix num s then Some(string_of_int n) else fn rest s (n+1)
+  in 
+  let get_digit str =
+    match str with
+    | "" -> None 
+    | s -> if is_digit s.[0] then Some(String.make 1 s.[0]) else fn nums s 1
+  in
+  let rec replace_all str =
+    match str with
+    | "" -> []
+    | s -> get_digit s :: replace_all (String.sub str 1 (String.length str - 1))
+  in 
+  replace_all str
+
+
+let rec print_l = function
+  | [] -> ()
+  | [x] -> Printf.printf "%s\n" x 
+  | x :: xs -> 
+      Printf.printf "%s; " x;
+      print_l xs
+  
+let print_option_list lst =
+  let print_option = function
+    | Some value -> Printf.printf "Some %s; " value
+    | None -> Printf.printf "None; "
+  in
+  List.iter print_option lst;
+  Printf.printf "\n"
+;;
 let a = read_file file
 let () = 
   let b = List.map (fun x -> int_of_string (get_concat x)) a in
   let c = List.fold_left (fun acc n -> acc + n) 0 b in
-  Printf.printf "%d\n" c
+  Printf.printf "%d\n" c;
+
+  let b = List.map (fun x -> assoc_nums x) a in 
+  let c = List.map (fun x -> (List.filter (fun y -> Option.is_some y) x)) b in
+  let d = List.map (fun x -> (List.map (fun y -> Option.get y) x)) c in
+  let e = List.map (fun x -> String.concat "" x) d in
+  let f = List.map (fun x -> int_of_string (get_concat x)) e in 
+  let g = List.fold_left (fun acc n -> acc + n) 0 f in 
+  Printf.printf "%d\n" g;
+
+
+ 
 
